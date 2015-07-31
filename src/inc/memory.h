a2ef26f873b077124f4f13f5e0923471b0db262c
DKavolis <daumantas.kavolis@gmail.com>
2015-07-31 17:41:24 +0100

    Added copy functions, functions now return cudaError_t

--- src/inc/memory.h
+++ src/inc/memory.h
@@ -10,36 +10,88 @@ class MemoryManagement
 {
 public:
     __host__ __device__ inline
-    void CleanUp(T * ptr)
+    static cudaError_t CleanUp(T * ptr)
     {
-        if (_onDevice) cudaFree(ptr);
-        else cudaFreeHost(ptr);
+        if (_onDevice) return cudaFree(ptr);
+        else return cudaFreeHost(ptr);
     }
 
     __host__ __device__ inline
-    void Malloc(T * &ptr, size_t & w, size_t & h, size_t &pitch)
+    static cudaError_t Malloc(T * &ptr, size_t & w, size_t & h, size_t &pitch)
     {
         if (_onDevice){
-            cudaMallocPitch(&ptr, &pitch, w * sizeof(T), h);
+            return cudaMallocPitch(&ptr, &pitch, w * sizeof(T), h);
         }
         else {
             pitch = w * sizeof(T);
-            cudaMallocHost(&ptr, pitch * h);
+            return cudaMallocHost(&ptr, pitch * h);
         }
     }
 
     __host__ __device__ inline
-    void Malloc(T * &ptr, size_t & w, size_t & h, size_t & d, size_t &pitch, size_t &spitch)
+    static cudaError_t Malloc(T * &ptr, size_t & w, size_t & h, size_t & d, size_t &pitch, size_t &spitch)
     {
+        cudaError_t err;
         if (_onDevice){
-            cudaMallocPitch(&ptr, &pitch, w * sizeof(T), h * d);
+            err = cudaMallocPitch(&ptr, &pitch, w * sizeof(T), h * d);
             spitch = h * pitch;
         }
         else {
             pitch = w * sizeof(T);
             spitch = h * pitch;
-            cudaMallocHost(&ptr, pitch * h * d);
+            err = cudaMallocHost(&ptr, pitch * h * d);
         }
+        return err;
+    }
+
+    // Copy 2D data structures:
+    __host__ __device__ inline
+    static cudaError_t Device2DeviceCopy(T * &pDst, size_t & DstPitch, T * &pSrc, size_t & SrcPitch, size_t width, size_t height)
+    {
+        return cudaMemcpy2D(pDst, DstPitch, pSrc, SrcPitch, width * sizeof(T), height, cudaMemcpyDeviceToDevice);
+    }
+
+    __host__ __device__ inline
+    static cudaError_t Device2HostCopy(T * &pDst, size_t & DstPitch, T * &pSrc, size_t & SrcPitch, size_t width, size_t height)
+    {
+        return cudaMemcpy2D(pDst, DstPitch, pSrc, SrcPitch, width * sizeof(T), height, cudaMemcpyDeviceToHost);
+    }
+
+    __host__ __device__ inline
+    static cudaError_t Host2DeviceCopy(T * &pDst, size_t & DstPitch, T * &pSrc, size_t & SrcPitch, size_t width, size_t height)
+    {
+        return cudaMemcpy2D(pDst, DstPitch, pSrc, SrcPitch, width * sizeof(T), height, cudaMemcpyHostToDevice);
+    }
+
+    __host__ __device__ inline
+    static cudaError_t Host2HostCopy(T * &pDst, size_t & DstPitch, T * &pSrc, size_t & SrcPitch, size_t width, size_t height)
+    {
+        return cudaMemcpy2D(pDst, DstPitch, pSrc, SrcPitch, width * sizeof(T), height, cudaMemcpyHostToHost);
+    }
+
+    // Copy 3D data structures
+    __host__ __device__ inline
+    static cudaError_t Device2DeviceCopy(T * &pDst, size_t & DstPitch, T * &pSrc, size_t & SrcPitch, size_t width, size_t height, size_t depth)
+    {
+        return cudaMemcpy2D(pDst, DstPitch, pSrc, SrcPitch, width * sizeof(T), height * depth, cudaMemcpyDeviceToDevice);
+    }
+
+    __host__ __device__ inline
+    static cudaError_t Device2HostCopy(T * &pDst, size_t & DstPitch, T * &pSrc, size_t & SrcPitch, size_t width, size_t height, size_t depth)
+    {
+        return cudaMemcpy2D(pDst, DstPitch, pSrc, SrcPitch, width * sizeof(T), height * depth, cudaMemcpyDeviceToHost);
+    }
+
+    __host__ __device__ inline
+    static cudaError_t Host2DeviceCopy(T * &pDst, size_t & DstPitch, T * &pSrc, size_t & SrcPitch, size_t width, size_t height, size_t depth)
+    {
+        return cudaMemcpy2D(pDst, DstPitch, pSrc, SrcPitch, width * sizeof(T), height * depth, cudaMemcpyHostToDevice);
+    }
+
+    __host__ __device__ inline
+    static cudaError_t Host2HostCopy(T * &pDst, size_t & DstPitch, T * &pSrc, size_t & SrcPitch, size_t width, size_t height, size_t depth)
+    {
+        return cudaMemcpy2D(pDst, DstPitch, pSrc, SrcPitch, width * sizeof(T), height * depth, cudaMemcpyHostToHost);
     }
 };
 
