486247ea7b16709fe784df46fabee4c9171a33bc
Daumantas Kavolis <daumantas.kavolis@gmail.com>
2015-08-03 15:55:38 +0100

    Depthmap fusion histogram update added

--- src/fusion.cu
+++ src/fusion.cu
@@ -2,15 +2,43 @@
 #include "fusion.cu.h"
 
 template<unsigned char _bins>
-__global__ void FusionUpdateHistogram_kernel(fusionData<_bins> * f, const float * __restrict__ depthmap, const float threshold)
+__global__ void FusionUpdateHistogram_kernel(fusionData<_bins> * f, const float * __restrict__ depthmap, const Matrix3D & K, const Matrix3D & R, const float3 & t, const float threshold, const int width, const int height)
 {
     const int x = threadIdx.x + blockDim.x * blockIdx.x;
     const int y = threadIdx.y + blockDim.y * blockIdx.y;
     const int z = threadIdx.z + blockDim.z * blockIdx.z;
+    const int i = x + f->width() * (y + z * f->height()); // element index
 
-    if ((x < f->width()) && (y < f->height()) && (z < f->depth()))
+    if (i < f->elements())
     {
+        // Get world coordinates of the voxel
+        float3 c = f->worldCoords(x, y, z);
 
+        // Transform world coordinates to camera coordinates
+        c = R * c + t;
+
+        // Transform camera coordinates to homogeneous pixel coordinates
+        c = K * c; // c.z - voxel depth in camera coordinates
+        float2 px = make_float2(c / c.z);
+
+        // Check if pixel coordinates fall inside image range
+        if ((px.x >= 0) && (px.x <= width-1) && (px.y >= 0) && (px.y <= height-1)) return;
+
+        // Get int pixel coords
+        int2 pxc = make_int2(floorf(px));
+
+        // Get fractions
+        float2 frac = fracf(px);
+
+        // Read image values for bilinterp
+        float2 y0 = make_float2(depthmap[pxc.x+pxc.y*width], depthmap[pxc.x+pxc.y*width+1]);
+        float2 y1 = make_float2(depthmap[pxc.x+(pxc.y+1)*width], depthmap[pxc.x+(pxc.y+1)*width]);
+
+        // Interpolate voxel depth
+        float depth = bilinterp(y0, y1, frac);
+
+        // Update histogram
+        f->updateHist(x, y, z, c.z, depth, threshold);
     }
 }
 
@@ -20,8 +48,9 @@ __global__ void FusionUpdateU_kernel(fusionData<_bins> * f, const double tau, co
     const int x = threadIdx.x + blockDim.x * blockIdx.x;
     const int y = threadIdx.y + blockDim.y * blockIdx.y;
     const int z = threadIdx.z + blockDim.z * blockIdx.z;
+    const int i = x + f->width() * (y + z * f->height()); // element index
 
-    if ((x < f->width()) && (y < f->height()) && (z < f->depth()))
+    if (i < f->elements())
     {
         const double un = f->u(x, y, z);
         const double u = un - tau * (- f->divPBwd(x, y, z));
@@ -36,8 +65,9 @@ __global__ void FusionUpdateP_kernel(fusionData<_bins> * f, const double sigma)
     const int x = threadIdx.x + blockDim.x * blockIdx.x;
     const int y = threadIdx.y + blockDim.y * blockIdx.y;
     const int z = threadIdx.z + blockDim.z * blockIdx.z;
+    const int i = x + f->width() * (y + z * f->height()); // element index
 
-    if ((x < f->width()) && (y < f->height()) && (z < f->depth()))
+    if (i < f->elements())
     {
         f->p(x, y, z) = f->projectUnitBall(f->p(x, y, z) + sigma * f->gradVFwd(x, y, z));
     }
@@ -55,10 +85,16 @@ void FusionUpdateP(fusionData<_bins> * f, const double sigma, dim3 blocks, dim3 
     FusionUpdateP_kernel<_bins><<<blocks, threads>>>(f, sigma);
 }
 
-// Missing histogram update step
-//template<unsigned char _bins>
-//void FusionUpdateIteration(fusionData<_bins> * f, const double tau, const double lambda, const double sigma, dim3 blocks, dim3 threads)
-//{
-//    FusionUpdateU_kernel<_bins><<<blocks, threads>>>(f, tau, lambda);
-//    FusionUpdateP_kernel<_bins><<<blocks, threads>>>(f, sigma);
-//}
+template<unsigned char _bins>
+void FusionUpdateHistogram(fusionData<_bins> * f, const float * depthmap, const Matrix3D & K, const Matrix3D & R, const float3 & t, const float threshold, const int width, const int height, dim3 blocks, dim3 threads)
+{
+    FusionUpdateHistogram_kernel<_bins><<<blocks, threads>>>(f, depthmap, K, R, t, threshold, width, height);
+}
+
+template<unsigned char _bins>
+void FusionUpdateIteration(fusionData<_bins> * f, const float * depthmap, const Matrix3D & K, const Matrix3D & R, const float3 & t, const float threshold, const double tau, const double lambda, const double sigma, const int width, const int height, dim3 blocks, dim3 threads)
+{
+    FusionUpdateHistogram_kernel<_bins><<<blocks, threads>>>(f, depthmap, K, R, t, threshold, width, height);
+    FusionUpdateU_kernel<_bins><<<blocks, threads>>>(f, tau, lambda);
+    FusionUpdateP_kernel<_bins><<<blocks, threads>>>(f, sigma);
+}
--- src/inc/fusion.cu.h
+++ src/inc/fusion.cu.h
@@ -21,6 +21,26 @@
 */
 
 /**
+ *  \brief Update histogram from given \a depthmap
+ *
+ *  \param f            pointer to \a fusionData containing histogram
+ *  \param depthmap     pointer to depthmap data
+ *  \param K            3x3 camera calibration matrix \f$K\f$
+ *  \param R            3x3 rotation matrix from world to camera coordinates
+ *  \param t            translation vector from world to camera position
+ *  \param threshold    signed distance value threshold
+ *  \param width        width of depthmap
+ *  \param height       height of depthmap
+ *  \param blocks       kernel grid dimensions
+ *  \param threads      single block dimensions
+ *  \return No return value
+ *
+ *  \details Signed distance is clamped to [-threshold,threshold] and divided by \a threshold before updating any histogram bins.
+ */
+template<unsigned char _bins>
+void FusionUpdateHistogram(fusionData<_bins> * f, const float * depthmap, const Matrix3D & K, const Matrix3D & R, const float3 & t, const float threshold, const int width, const int height, dim3 blocks, dim3 threads);
+
+/**
  *  \brief Update primal variable \f$u\f$ and helper variable \f$v\f$ using histogram depthmap fusion algorithm
  *
  *  \param f       pointer to \a fusionData containing \f$u\f$
@@ -52,18 +72,25 @@ void FusionUpdateP(fusionData<_bins> * f, const double sigma, dim3 blocks, dim3 
 /**
  *  \brief Single depthmap fusion iteration function
  *
- *  \param f       pointer to \a fusionData
- *  \param tau     fusion parameter \f$\tau\f$
- *  \param lambda  fusion parameter \f$\lambda\f$
- *  \param sigma   fusion parameter \f$\sigma\f$
- *  \param blocks  kernel grid dimensions
- *  \param threads single block dimensions
+ *  \param f            pointer to \a fusionData
+ *  \param depthmap     pointer to depthmap to be used in updating histograms
+ *  \param K            3x3 camera calibration matrix \f$K\f$
+ *  \param R            3x3 rotation matrix from world to camera coordinates
+ *  \param t            translation vector from world to camera position
+ *  \param threshold    signed distance value threshold
+ *  \param tau          fusion parameter \f$\tau\f$
+ *  \param lambda       fusion parameter \f$\lambda\f$
+ *  \param sigma        fusion parameter \f$\sigma\f$
+ *  \param width        width of depthmap
+ *  \param height       height of depthmap
+ *  \param blocks       kernel grid dimensions
+ *  \param threads      single block dimensions
  *  \return No return value
  *
- *  \details <b>MISSING HISTOGRAM UPDATE STEP</b>
+ *  \details Signed distance is clamped to [-threshold,threshold] and divided by \a threshold before updating any histogram bins.
  */
 template<unsigned char _bins>
-void FusionUpdateIteration(fusionData<_bins> * f, const double tau, const double lambda, const double sigma, dim3 blocks, dim3 threads);
+void FusionUpdateIteration(fusionData<_bins> * f, const float * depthmap, const Matrix3D & K, const Matrix3D & R, const float3 & t, const float threshold, const double tau, const double lambda, const double sigma, const int width, const int height, dim3 blocks, dim3 threads);
 
 /** @} */ // group fusion
 
