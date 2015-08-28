a54443288c3255b11c0ea4ca6e0ba848df82a6a8
DKavolis <daumantas.kavolis@gmail.com>
2015-08-28 17:24:05 +0100

    added ctors

--- src/inc/cam_image.h
+++ src/inc/cam_image.h
@@ -7,10 +7,53 @@
 template<typename T>
 struct CamImage : public Image<T, Standard>
 {
-    virtual ~CamImage() { free(); }
-
     Matrix3D R;
     Vector3D t;
+
+    virtual ~CamImage() { free(); }
+
+    template<MemoryKind memT>
+    inline __host__ __device__
+    CamImage( const Image<T,memT>& img )
+        : Image(img), R(), t()
+    {}
+
+    inline __host__ __device__
+    CamImage( const CamImage<T>& img )
+        : Image(img), R(img.R), t(img.t)
+    {}
+
+    inline __host__
+    CamImage()
+        : Image(), R(), t()
+    {}
+
+    inline __host__
+    CamImage(size_t w, size_t h)
+        : Image(w, h), R(), t()
+    {
+       Malloc(ptr_, w_, h_, pitch_);
+    }
+
+    inline __device__ __host__
+    CamImage(T* ptr)
+        : Image(ptr), R(), t()
+    {}
+
+    inline __device__ __host__
+    CamImage(T* ptr, size_t w)
+        : Image(ptr, w), R(), t()
+    {}
+
+    inline __device__ __host__
+    CamImage(T* ptr, size_t w, size_t h)
+        : Image(ptr, w, h), R(), t()
+    {}
+
+    inline __device__ __host__
+    CamImage(T* ptr, size_t w, size_t h, size_t pitch)
+        : Image(ptr, w, h, pitch), R(), t()
+    {}
 };
 
 #endif // CAM_IMAGE_H
